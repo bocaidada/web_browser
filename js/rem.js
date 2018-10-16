c2a9a13f600b73e657729b3d55beacedd119c82a
bocai <31608673+bocaidada@users.noreply.github.com>
2018-10-16 14:13:32 +0800

    Update rem.js

--- js/rem.js
+++ js/rem.js
@@ -1,21 +1,33 @@
-(function (doc, win) {
-        var docEl = doc.documentElement,
-            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-            recalc = function () {
-                var clientWidth = docEl.clientWidth;
-                if (!clientWidth) return;
-                if(clientWidth>=750){
-                  // 这里的750 取决于设计稿的宽度
-                    docEl.style.fontSize = '100px';
-                }else{
-                    docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-                }
-            };
+// (function (doc, win) {
+//         var docEl = doc.documentElement,
+//             resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+//             recalc = function () {
+//                 var clientWidth = docEl.clientWidth;
+//                 if (!clientWidth) return;
+//                 if(clientWidth>=750){
+//                   // 这里的750 取决于设计稿的宽度
+//                     docEl.style.fontSize = '100px';
+//                 }else{
+//                     docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+//                 }
+//             };
 
-        if (!doc.addEventListener) return;
-        win.addEventListener(resizeEvt, recalc, false);
-        doc.addEventListener('DOMContentLoaded', recalc, false);
-    })(document, window);
+//         if (!doc.addEventListener) return;
+//         win.addEventListener(resizeEvt, recalc, false);
+//         doc.addEventListener('DOMContentLoaded', recalc, false);
+//     })(document, window);
 
 
 
+function resize() {
+    var docEl = document.documentElement;
+    var clientWidth = window.innerWidth;
+    if(clientWidth<=375){
+        docEl.style.fontSize='50px';
+    }else if(clientWidth>=750){
+        docEl.style.fontSize='100px';
+    }else{
+        docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+    }
+}
+resize();
